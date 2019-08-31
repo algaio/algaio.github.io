07166df28979fc2c36c7c8940459de094db1f792
Alan Ionita <a.g.ionita@gmail.com>
2019-08-31 21:19:21 +0100

    FEAT : Updates the previous index page to look like the previous portfolio homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'gatsby';
+import Segment from '../components/segment/segment';
+import SEO from '../components/seo';
+import segmentsData from '../data/segments.json';
+import '../styles/__index.scss';
+import './index.scss';
 
 const IndexPage = () => (
-  <Layout>
+  <main className="app">
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <section>
+      {segmentsData.segments.map(segment => (
+        <Segment data={segment} key={segment.id} />
+      ))}
+    </section>
+  </main>
+);
 
-export default IndexPage
+export default IndexPage;
