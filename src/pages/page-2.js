5da3b49386d8ac2d89033efa4c47558e889d87d5
Alan Ionita <a.g.ionita@gmail.com>
2019-08-31 21:14:43 +0100

    REFACTOR : Removed previous componets of the Gatsby template

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from '../components/seo';
 
 const SecondPage = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+  </React.Fragment>
+);
 
-export default SecondPage
+export default SecondPage;
