9242df74114963873060c3e9e913988bae98f114
Alan Ionita <a.g.ionita@gmail.com>
2019-12-14 02:26:51 +0000

    FIX : updates config, remove gh-pages script and updates the scripts to correct deployment ones

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: 'Alan Ionita Portfolio',
-    description: 'Portfolio and blog site for Alan Ionita',
-    author: 'Alan Ionita',
+    title: "Alan Ionita Portfolio",
+    description: "Portfolio and blog site for Alan Ionita",
+    author: "Alan Ionita",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'alan-ionita-portfolio',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/cyclist.png', // This path is relative to the root of the site.
+        name: "alan-ionita-portfolio",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/cyclist.png", // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-};
+}
