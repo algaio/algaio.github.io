2a27c59701faa3b7f49f7ca0ba5fe4f705fdfa3e
Alan Ionita <a.g.ionita@gmail.com>
2020-08-01 13:57:02 +0100

    FEAT : (home) adds data for the homepage
    Implements the Segment component on the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/Layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPosts } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/Date";
+import Segment from "../components/Segment";
+import Layout, { siteTitle } from "../components/Layout";
+import { getSortedPosts } from "../lib/posts";
 import { Posts } from "../types/posts";
+import { content } from "../data/indexContent.json";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home({ allPosts }: { allPosts: Posts }) {
   return (
@@ -19,6 +21,11 @@ export default function Home({ allPosts }: { allPosts: Posts }) {
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
+        <section>
+          {content.map((segment) => (
+            <Segment data={segment} key={segment.id} />
+          ))}
+        </section>
       </section>
     </Layout>
   );
