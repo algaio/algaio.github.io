0cc94c58e9fd42415e387483d103a6917b210220
Alan Ionita <a.g.ionita@gmail.com>
2020-08-01 23:29:53 +0100

    REFACTOR : (pages/home) improves the markup and cleans up the component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,26 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
-// import Link from "next/link";
-// import Date from "../components/Date";
+
+// Components
 import Segment from "../components/Segment";
 import Layout, { siteTitle } from "../components/Layout";
-import { getSortedSegments } from "../lib/segments";
-import { Segments } from "../types/segments";
-import utilStyles from "../styles/utils.module.css";
 
-export default function Home({ allSegments }: { allSegments: Segments }) {
-  console.log("allSegments", allSegments);
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      {/* <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-        </p>
-        
-      </section> */}
-      <section>
-        {allSegments.map((segment) => (
-          <Segment {...segment} key={segment.id} />
-        ))}
-      </section>
-    </Layout>
-  );
-}
+// Data
+import { getSortedSegments } from "../lib/segments";
+
+// TS
+import { Segments } from "../types/segments";
+
+export const Home = ({ allSegments }: { allSegments: Segments }) => (
+  <Layout home>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    {allSegments.map((segment) => (
+      <Segment {...segment} key={segment.id} />
+    ))}
+  </Layout>
+);
 
 export const getStaticProps: GetStaticProps = async () => {
   const allSegments = await getSortedSegments("home");
