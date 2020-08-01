a952a60f3115f507c9a5cf6758fad1ee94d814bf
Alan Ionita <a.g.ionita@gmail.com>
2020-08-01 19:21:32 +0100

    FIX : updates the config and adds a .nojekyll file to deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  target: "serverless",
-  trailingSlash: true,
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? "/alanionita.github.io" : "",
+  
 };
