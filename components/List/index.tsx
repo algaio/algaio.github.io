74f86a508b579304036127a8a9c26dc9d90ab16f
Alan Ionita <a.g.ionita@gmail.com>
2020-08-01 23:29:17 +0100

    FEAT : (List) adds new markup and no items fallback to List comp

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -1,15 +1,31 @@
-import styles from "./list.module.css";
+import Link from "next/link";
+import { Posts } from "../../types/posts";
 
-const List = ({ list }: { list: Array<any> }) => (
+// Components
+import Date from "../Date";
+
+// Styles
+import styles from "./list.module.css";
+import utilStyles from "../../styles/utils.module.css";
+
+const List = ({ list }: { list: Posts }) => (
   <>
-    {list &&
-      list.length > 0 &&
-      list.map((item) => (
-        <section className={styles.item}>
-          <a href={item.href}>{item.text}</a>
-          <p>, {item.date}</p>
-        </section>
-      ))}
+    {list && (
+      <ul className={utilStyles.list}>
+        {list.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            <Link href='/posts/[id]' as={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+        ))}
+      </ul>
+    )}
+    {list && list.length < 1 && <p>No available articles, but stay tuned!</p>}
   </>
 );
 
