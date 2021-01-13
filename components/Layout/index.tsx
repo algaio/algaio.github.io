5bfdd50c4a74806d0aff85794b8d5108dcc47fc0
Alan Ionita <a.g.ionita@gmail.com>
2021-01-13 19:45:03 +0000

    FIX : temporarily hides the og:image until I create a proper system for dynamically dealing with that

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,54 +3,49 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "./layout.module.css";
 import Nav from "../Nav";
-import utilStyles from "../../styles/utils.module.css";
-const name = "[Your Name]";
-export const siteTitle = "Next.js Sample Website";
+
+export const siteTitle = "Alan Ionita - Fullstack developer from Manchester 🇬🇧";
 
 type LayoutProps = {
   home: boolean;
 };
 
-const Layout: FunctionComponent<LayoutProps> = ({ children, home }) => {
-  const LinkProps = {
-    href: "/",
-  };
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel='icon' href='/favicon.ico' />
-        <meta
-          name='description'
-          content='Learn how to build a personal website using Next.js'
-        />
-        <meta
-          property='og:image'
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name='og:title' content={siteTitle} />
-        <meta name='twitter:card' content='summary_large_image' />
-      </Head>
-      <header className={styles.header}>
-        {home && (
-          <>
-            <Nav>
-              <Link href='/blog'>Blog</Link>
-            </Nav>
-          </>
-        )}
-        {!home && (
-          <>
-            <Nav>
-              <Link href='/'>Home</Link>
-            </Nav>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-    </div>
-  );
-};
+const Layout: FunctionComponent<LayoutProps> = ({ children, home }) => (
+  <div className={styles.container}>
+    <Head>
+      <link rel='icon' href='/favicon.ico' />
+      <meta
+        name='description'
+        content='Learn how to build a personal website using Next.js'
+      />
+      {/* <meta
+        property='og:image'
+        // content={`https://og-image.now.sh/${encodeURI(siteTitle)}.png`}
+        content={`https://u0oikxk9t0.execute-api.eu-west-2.amazonaws.com/dev/${encodeURI(
+          siteTitle
+        )}.png`}
+      /> */}
+      <meta name='og:title' content={siteTitle} />
+      <meta name='twitter:card' content='summary_large_image' />
+    </Head>
+    <header className={styles.header}>
+      {home && (
+        <>
+          <Nav>
+            <Link href='/blog'>Blog</Link>
+          </Nav>
+        </>
+      )}
+      {!home && (
+        <>
+          <Nav>
+            <Link href='/'>Home</Link>
+          </Nav>
+        </>
+      )}
+    </header>
+    <main>{children}</main>
+  </div>
+);
 
 export default Layout;
